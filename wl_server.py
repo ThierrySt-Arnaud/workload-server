c44e780c2293cae9ad114fb5f03a897e8c14b605
ThierrySt-Arnaud <haratiri@gmail.com>
2019-10-27 18:08:54 -0400

    Changed server to listen on external port, for real this time

--- wl_server.py
+++ wl_server.py
@@ -1,14 +1,12 @@
 import asyncio
 from workload_server import wl_db, rfw_tcp_server
-import requests
 
-IPIFY_URL = 'https://api.ipify.org'
+IP = "0.0.0.0"
 
 
 async def main():
     wl_db.initialize_database()
-    ip = requests.get('https://api.ipify.org').text
-    async with await rfw_tcp_server.start_rfw_server(host=ip) as server:
+    async with await rfw_tcp_server.start_rfw_server(host=IP) as server:
         await server.serve_forever()
 
 if __name__ == "__main__":
